0db22bcd214037b235ebe14d1566cb6d6185cc40
Pasang24 <pasangtheeng14@gmail.com>
2025-05-11 22:31:37 +0545

    feat: complete google OAuth login functionality and return user excluding the password in email login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,6 +6,13 @@ import { and, eq } from "drizzle-orm";
 import db from "../db";
 
 type User = typeof user.$inferSelect;
+type TokenData = {
+  access_token: string;
+  expires_in: number;
+  scope: string;
+  token_type: string;
+  id_token: string;
+};
 
 const emailLogin = async (
   req: Request<{}, {}, Pick<User, "email" | "password">, {}>,
@@ -45,7 +52,9 @@ const emailLogin = async (
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   });
 
-  res.json({ user: currentUser });
+  const { password: _, ...safeUser } = currentUser;
+
+  res.json(safeUser);
 };
 
 const googleLogin = async (req: Request, res: Response) => {
@@ -66,9 +75,69 @@ const googleLoginCallback = async (
   res: Response
 ) => {
   const { code } = req.query;
-  console.log(code);
 
-  res.send("Code received");
+  const tokenRes = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      code,
+      client_id: process.env.GOOGLE_CLIENT_ID as string,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirect_uri: "http://localhost:4000/auth/google/callback",
+      grant_type: "authorization_code",
+    }),
+  });
+
+  const tokenData: TokenData = await tokenRes.json();
+  const accessToken = tokenData.access_token;
+
+  const profileRes = await fetch(
+    "https://www.googleapis.com/oauth2/v2/userinfo",
+    {
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  const profile: Pick<User, "email" | "name"> = await profileRes.json();
+
+  const result = await db
+    .insert(user)
+    .values({
+      name: profile.name,
+      email: profile.email,
+      provider: "google",
+    })
+    .onConflictDoNothing({ target: [user.email, user.provider] })
+    .returning();
+
+  let currentUser = result[0];
+
+  if (!currentUser) {
+    const response = await db
+      .select()
+      .from(user)
+      .where(and(eq(user.email, profile.email), eq(user.provider, "google")));
+
+    currentUser = response[0];
+
+    const sessionToken = jwt.sign(
+      { id: currentUser.id },
+      process.env.JWT_SECRET as jwt.Secret
+    );
+
+    res.cookie("session", sessionToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    });
+
+    console.log(currentUser);
+
+    // replace the redirect url with your frontend url
+    res.redirect("http://localhost:3000");
+  }
 };
 
 export const authController = { emailLogin, googleLogin, googleLoginCallback };
