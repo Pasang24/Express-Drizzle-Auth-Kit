c90d45b6dc3615760090c2afcdd26b57d210f7c2
Pasang24 <pasangtheeng14@gmail.com>
2025-05-11 22:30:11 +0545

    fix: remove unique constraint from email column and add it to the combination of email and provider column

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,13 +1,24 @@
-import { pgTable, varchar, uuid, timestamp, pgEnum } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  varchar,
+  uuid,
+  timestamp,
+  pgEnum,
+  unique,
+} from "drizzle-orm/pg-core";
 
 export const providerEnum = pgEnum("provider", ["email", "google", "github"]);
 
-export const user = pgTable("user", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: varchar("name", { length: 255 }).notNull(),
-  email: varchar("email", { length: 255 }).unique().notNull(),
-  password: varchar("password", { length: 255 }),
-  provider: providerEnum("provider").default("email").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at"),
-});
+export const user = pgTable(
+  "user",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    password: varchar("password", { length: 255 }),
+    provider: providerEnum("provider").default("email").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at"),
+  },
+  (table) => [unique().on(table.email, table.provider)]
+);
