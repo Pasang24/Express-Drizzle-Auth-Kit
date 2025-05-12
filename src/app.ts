12a0ee7c5e22a897dadec23164469a11a89b14ce
Pasang24 <pasangtheeng14@gmail.com>
2025-05-12 10:32:05 +0545

    feat: setup cors and add frontend url as origin

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import "dotenv/config";
 import express from "express";
+import cors, { CorsOptions } from "cors";
 import cookieParser from "cookie-parser";
 import { authRoutes } from "./routes/auth.route";
 
 const app = express();
 
+const corsOptions: CorsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "UPDATE", "DELETE", "PUT", "PATCH", "OPTIONS"],
+  credentials: true,
+};
+
 //middlewares
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
