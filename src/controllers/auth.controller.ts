7f8fd7f510257e7cc83123f6999c0fe06d0b234c
Pasang24 <pasangtheeng14@gmail.com>
2025-05-12 14:03:14 +0545

    feat: add the global error handler in auth controller using try/catch block

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
+import { ApiError } from "../utils/ApiError";
 import { generateSession } from "../utils/generateSession";
 import { user } from "../db/schema/user";
 import { and, eq } from "drizzle-orm";
@@ -16,36 +17,40 @@ type TokenData = {
 
 const emailLogin = async (
   req: Request<{}, {}, Pick<User, "email" | "password">, {}>,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
-  const { email, password } = req.body;
-  const result = await db
-    .select()
-    .from(user)
-    .where(and(eq(user.email, email), eq(user.provider, "email")));
+  try {
+    const { email, password } = req.body;
 
-  const currentUser = result[0];
+    const result = await db
+      .select()
+      .from(user)
+      .where(and(eq(user.email, email), eq(user.provider, "email")));
 
-  if (!currentUser) {
-    res.status(401).json({ message: "Invalid Email or Password" });
-    return;
+    const currentUser = result[0];
+
+    if (!currentUser) {
+      throw new ApiError(401, "Invalid Email or Password");
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      password as string,
+      currentUser.password as string
+    );
+
+    if (!isPasswordMatch) {
+      throw new ApiError(401, "Invalid Email or Password");
+    }
+
+    generateSession(res, currentUser.id);
+
+    const { password: _, ...safeUser } = currentUser;
+
+    res.json({ user: safeUser });
+  } catch (error) {
+    next(error);
   }
-
-  const isPasswordMatch = await bcrypt.compare(
-    password as string,
-    currentUser.password as string
-  );
-
-  if (!isPasswordMatch) {
-    res.status(401).json({ message: "Invalid Email or Password" });
-    return;
-  }
-
-  generateSession(res, currentUser.id);
-
-  const { password: _, ...safeUser } = currentUser;
-
-  res.json({ user: safeUser });
 };
 
 const googleLogin = async (req: Request, res: Response) => {
@@ -63,74 +68,75 @@ const googleLogin = async (req: Request, res: Response) => {
 
 const googleLoginCallback = async (
   req: Request<{}, {}, {}, { code: string }>,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
-  const { code } = req.query;
-
-  // if the user tries to access the callback route directly without getting redirected
-  // we will not have code in the request query so we return it as a bad request
-  if (!code) {
-    res.status(400).json({ message: "Missing authorization code" });
-    return;
-  }
-
-  const tokenRes = await fetch("https://oauth2.googleapis.com/token", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({
-      code,
-      client_id: process.env.GOOGLE_CLIENT_ID as string,
-      client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
-      redirect_uri: `${process.env.BACKEND_URL}/auth/google/callback`,
-      grant_type: "authorization_code",
-    }),
-  });
-
-  if (!tokenRes.ok) {
-    const errorData = await tokenRes.json();
-    console.log(errorData);
-    res.status(400).json({ message: "Invalid or expired code" });
-    return;
-  }
-
-  const tokenData: TokenData = await tokenRes.json();
-  const accessToken = tokenData.access_token;
-
-  const profileRes = await fetch(
-    "https://www.googleapis.com/oauth2/v2/userinfo",
-    {
-      headers: {
-        "Authorization": `Bearer ${accessToken}`,
-      },
+  try {
+    const { code } = req.query;
+    // if the user tries to access the callback route directly without getting redirected
+    // we will not have code in the request query so we return it as a bad request
+    if (!code) {
+      throw new ApiError(400, "Missing authorization code");
     }
-  );
 
-  const profile: Pick<User, "email" | "name"> = await profileRes.json();
+    const tokenRes = await fetch("https://oauth2.googleapis.com/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        code,
+        client_id: process.env.GOOGLE_CLIENT_ID as string,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
+        redirect_uri: `${process.env.BACKEND_URL}/auth/google/callback`,
+        grant_type: "authorization_code",
+      }),
+    });
 
-  const result = await db
-    .insert(user)
-    .values({
-      name: profile.name,
-      email: profile.email,
-      provider: "google",
-    })
-    .onConflictDoNothing({ target: [user.email, user.provider] })
-    .returning();
+    if (!tokenRes.ok) {
+      const errorData = await tokenRes.json();
+      throw new ApiError(400, "Invalid or expired code");
+    }
 
-  let currentUser = result[0];
+    const tokenData: TokenData = await tokenRes.json();
+    const accessToken = tokenData.access_token;
 
-  if (!currentUser) {
-    const response = await db
-      .select()
-      .from(user)
-      .where(and(eq(user.email, profile.email), eq(user.provider, "google")));
+    const profileRes = await fetch(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        headers: {
+          "Authorization": `Bearer ${accessToken}`,
+        },
+      }
+    );
 
-    currentUser = response[0];
+    const profile: Pick<User, "email" | "name"> = await profileRes.json();
 
-    generateSession(res, currentUser.id);
+    const result = await db
+      .insert(user)
+      .values({
+        name: profile.name,
+        email: profile.email,
+        provider: "google",
+      })
+      .onConflictDoNothing({ target: [user.email, user.provider] })
+      .returning();
 
-    // replace the redirect url with your frontend url
-    res.redirect(`${process.env.FRONTEND_URL}`);
+    let currentUser = result[0];
+
+    if (!currentUser) {
+      const response = await db
+        .select()
+        .from(user)
+        .where(and(eq(user.email, profile.email), eq(user.provider, "google")));
+
+      currentUser = response[0];
+
+      generateSession(res, currentUser.id);
+
+      // replace the redirect url with your frontend url
+      res.redirect(`${process.env.FRONTEND_URL}`);
+    }
+  } catch (error) {
+    next(error);
   }
 };
 
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -10,8 +10,6 @@ export const errorHandler = (
   const statusCode = err instanceof ApiError ? err.statusCode : 500;
   const message = err.message || "Internal Server Error";
 
-  console.log(err);
-
   res.status(statusCode).json({
     success: false,
     message,
